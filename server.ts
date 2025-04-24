6669646704a2d6142bb2ddd8a96687bc0fd40eed
Possomato <gabrielb.possomato@gmail.com>
2025-04-24 14:24:24 -0300

    exercise 3

--- server.ts
+++ server.ts
@@ -1,26 +1,11 @@
-const input = document.querySelector('input')
-const response = document.querySelector('p')
-
-const inputLocal = localStorage.getItem('input')
-if(input && inputLocal){
-  input.value = inputLocal
-  handleCalculate(Number(inputLocal))
-}
-
-function valueChanged(){
-  if(input){
-    let inputNumber = input.value
-    localStorage.setItem('input', inputNumber)
-    handleCalculate(Number(inputNumber))
+function toNumber(value: number | string){
+  if(typeof value === 'number'){
+    return value
+  } else if (typeof value === 'string'){
+    return Number(value)
+  } else{
+    throw 'error - value accepts only number or string'
   }
 }
 
-function handleCalculate(value: number){
-  if(response){
-    response.innerHTML = `result: ${value * 0.8 + 100}`
-  }
-}
-
-if(input){
-  input.addEventListener('keyup', valueChanged)
-}
+console.log(toNumber(9))
